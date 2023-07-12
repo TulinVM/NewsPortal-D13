a38de242278ec0f0ceae68a770a3a4c2ccefdf4f
TulinVM <tulinvm@gmail.com>
2023-07-12 22:52:25 +0300

    Update ReadMe.md menu subscriptions Документация

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -75,8 +75,20 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'news.middlewares.TimezoneMiddleware', # add that middleware!
+    'news.middlewares.TimezoneMiddleware', # add that middleware
 ]
+"""
+MIDDLEWARE = [
+'django.middleware.security.SecurityMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.locale.LocaleMiddleware',
+'django.middleware.common.CommonMiddleware',
+'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+'django.middleware.clickjacking.XFrameOptionsMiddleware'
+]
+"""
 
 ROOT_URLCONF = 'NewsPaper.urls'
 
